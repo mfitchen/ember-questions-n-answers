d5d17361379e200edbb7c637d2c81e42827344e1
Marty Fitchen <mfitchen@gmail.com>
2016-08-18 18:14:58 -0700

    add completed new question js file

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+
+    newFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    save1() {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+        detail: this.get('detail')
+      };
+      this.set('question', '');
+      this.set('author', '');
+      this.set('addNewPost', false);
+      this.sendAction('save2', params);
+    }
+  }
 });

50fb0c3a61ee15cbe21ab5b849871be8b858335d
Marty Fitchen <mfitchen@gmail.com>
2016-08-18 20:09:07 -0700

    add working files to link a question to its dynamic route and display the question on that page with a button to reveal the form to add an answer

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showNewAnswerForm: false,
+
+  actions: {
+    showNewForm(){
+      this.set('showNewAnswerForm', true);
+    },
+
+    save1(){
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        question: this.get('question')
+      };
+      this.set('showNewAnswerForm', false);
+      this.sendAction('save1', params);
+    }
+  }
 });

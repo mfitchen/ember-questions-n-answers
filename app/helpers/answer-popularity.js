955d2f2cb0a71ce1efbbeea535db56d455789cd6
Marty Fitchen <mfitchen@gmail.com>
2016-08-26 08:55:10 -0700

    add working files with styledstar icon when a question has 2 or more answers

--- app/helpers/answer-popularity.js
+++ app/helpers/answer-popularity.js
@@ -4,7 +4,7 @@ export function answerPopularity(params) {
   var qNa = params[0];
 
   if(qNa.get('answers').get('length') >= 2) {
-    return Ember.String.htmlSafe('<i class="fa fa-money" aria-hidden="true"></i>');
+    return Ember.String.htmlSafe('<i class="fa fa-star-o that-question-is-gonna-be-a-star" aria-hidden="true"></i>');
   }
 }
 

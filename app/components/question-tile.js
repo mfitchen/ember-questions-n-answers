4a56675dd5c53abf932fae690efb019826cb4066
Marty Fitchen <mfitchen@gmail.com>
2016-08-26 10:35:04 -0700

    add working files with a computed property that displays how many answers a question has

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favoriteQuestions: Ember.inject.service(),
 
+  answerCount: Ember.computed('question.answers', function() {
+    var answerAmount = this.get('question.answers.length');
+    return answerAmount;
+  }),
+
   actions: {
     addToFavs(question) {
       this.get('favoriteQuestions').add(question);

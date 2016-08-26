9f8c441e3a4ba5b5def9451dbf6b63b1010d8a52
Marty Fitchen <mfitchen@gmail.com>
2016-08-25 19:09:03 -0700

    add a helper for questions with more than 2 questions being popular

---
+++ tests/unit/helpers/answer-popularity-test.js
@@ -0,0 +1,10 @@
+import { answerPopularity } from 'questions-answers/helpers/answer-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | answer popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = answerPopularity([42]);
+  assert.ok(result);
+});
